22314d03cd2147d8c64d9388ca178cb6e69bb81f
Jesús Celemín <jesuscelemin.s@gmail.com>
2018-01-29 18:30:54 +0100

    Añadidas las categorias al menú

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
+import * as WC from 'woocommerce-api';
 
 @Component({
   selector: 'page-menu',
@@ -9,12 +10,50 @@ import { HomePage } from '../home/home';
 export class MenuPage {
 
   homePage: Component;
+  WooCommerce: any;
+  categories: any[];
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams)
     {
       this.homePage = HomePage;
+      this.categories = [];
+
+      this.WooCommerce = WC({
+        url: 'http://localhost:8888/',
+        consumerKey: 'ck_33659245c8ad0bab4000b38102b74f232bd1437e',
+        consumerSecret: 'cs_3822be29f442c1fa0ead5e872c30ce183fe1d8c6',
+        wpAPI: true, // Enable the WP REST API integration
+        version: 'wc/v2', // WooCommerce WP REST API version
+        queryStringAuth: true,
+      });
+
+      this.WooCommerce.getAsync("products/categories").then((data) => {
+        console.log(JSON.parse(data.body));
+
+        let temp: any[] = JSON.parse(data.body);
+
+        for( let i = 0; i < temp.length; i ++) {
+          if(temp[i].parent == 0) {
+
+            if (temp[i].slug == "clothing") {
+              temp[i].icon = "shirt";
+            }
+            if (temp[i].slug == "music") {
+              temp[i].icon = "musical-notes";
+            }
+            if (temp[i].slug == "posters") {
+              temp[i].icon = "images";
+            }
+
+            this.categories.push(temp[i]);
+          }
+        }
+
+      }, (err) => {
+        console.log(err);
+      });
   }
 
   ionViewDidLoad() {
